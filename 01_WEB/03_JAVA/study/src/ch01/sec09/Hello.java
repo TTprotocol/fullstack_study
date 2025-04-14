86bfcb4ac89da6bcc9d4866ec26ee7322170dec0
TTprotocol <wnsdud7941@gmail.com>
2025-04-14 12:44:37 +0900

    # java 시작

---
+++ 01_WEB/03_JAVA/study/src/ch01/sec09/Hello.java
@@ -0,0 +1,9 @@
+package ch01.sec09; // 어느 패키기에 있는지 항상 표시됨.
+
+public class Hello {
+// Hello 클래스 생성됨. => 파일 명과 클래스 명이 반드시 일치해야 함.
+    // 시작은 항상 main에서 시작. = entry point
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
